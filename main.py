d2e5d8177d1e60fd42008cee0b6c13ed9d32bf63
Jeremy Song <1678342+jeremysong@users.noreply.github.com>
2023-08-19 15:01:03 -0700

    Init commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import redis
+
+if __name__ == "__main__":
+    r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+    key = r.hset(
+        "11111111",
+        mapping = {
+            "111": "value of 111",
+            "222": "value of 222",
+            "333": "value of 333",
+        }
+    )
+    print("Get key 111 for CID 11111111", r.hget("11111111", "111"))
+    print("Get key 111, 222 for CID 11111111", r.hmget("11111111", ["111", "222"]))
+
+    print("Updating value for key 111")
+    r.hset("11111111", mapping = {
+        "111": "Updated value of 111"
+    })
+    print("Get key 111 for CID 11111111", r.hget("11111111", "111"))
+    print("Get all values for CID 11111111", r.hgetall("11111111"))
+
